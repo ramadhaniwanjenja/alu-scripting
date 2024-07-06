77b8006d3f6f449d12077f0a1c7ff84393a61c70
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-07-06 14:29:50 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
-function that queries the 'Reddit API' and returns the number of subscribers
+This module contains a function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
 import requests
 
 
-def number_of_subscribers(Subreddit):
+def number_of_subscribers(subreddit):
     """
-    number of subscribers
+    Return the number of subscribers for a given subreddit.
+    If the subreddit is invalid, return 0.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(Subreddit)
-    headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
+    
+    print("Status Code:", response.status_code)  # Debugging line
     if response.status_code == 200:
-        data = response.json()["data"]
-        return data["subscribers"]
-    else:
-        return 0
+        data = response.json()
+        print("Response JSON:", data)  # Debugging line
+        return data.get("data", {}).get("subscribers", 0)
+    return 0
