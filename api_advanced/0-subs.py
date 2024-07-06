e1245f1052d41e20b05c2361ec1b61ef5315c681
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-07-06 14:42:08 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json().get("data", {})
         return data.get("subscribers", 0)
