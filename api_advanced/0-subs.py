a106ab933ab59395697bee00a4ab2a548a17d3dc
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-07-06 14:23:14 -0700

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,11 +5,11 @@ function that queries the 'Reddit API' and returns the number of subscribers
 import requests
 
 
-def number_of_subscribers(subreddit):
+def number_of_subscribers(Subreddit):
     """
     number of subscribers
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(Subreddit)
     headers = {"User-Agent": "MyBot/1.0"}  # avoid Too Many Requests error
     response = requests.get(url, headers=headers, allow_redirects=False)
 
