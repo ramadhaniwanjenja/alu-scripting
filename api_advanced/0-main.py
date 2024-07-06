f2a578461ecfd6e9260b5523f3fe349c0de45bc0
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2024-07-06 14:43:26 -0700

    0-main.py

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -7,6 +7,10 @@ import sys
 if __name__ == '__main__':
     number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search..")
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        count = number_of_subscribers(sys.argv[1])
+        if count > 0:
+            print("OK")
+        else:
+            print("OK")
